20c319ab91063f84ead6768c596f0eab4d07d09f
Marc Blattmann <marc.blattmann08@gmail.com>
2025-11-01 21:22:27 +0100

    NPM Package to accses data

---
+++ package/index.js
@@ -0,0 +1,41 @@
+const systems = require('../core/data/systems.json');
+
+function getAllSystems() {
+  return systems;
+}
+
+function getSystemById(id) {
+  return systems.find(s => s.id === id);
+}
+
+function convertToPercent(systemId, grade) {
+  const system = getSystemById(systemId);
+  if (!system) throw new Error('System not found');
+  
+  const firstGrade = system.mappings[0].grade;
+  const lastGrade = system.mappings[system.mappings.length - 1].grade;
+  
+  return ((grade - firstGrade) / (lastGrade - firstGrade)) * 100;
+}
+
+function getColor(systemId, grade) {
+  const system = getSystemById(systemId);
+  const percent = convertToPercent(systemId, grade);
+
+  if (percent < system.passing_percent) return 'red';
+  if (percent < system.satisfactory_percent) return 'orange';
+  return 'green';
+}
+
+function getGradeDescription(systemId, grade) {
+  const system = getSystemById(systemId);
+  let closest = system.mappings[0];
+  for (const m of system.mappings) {
+    if (Math.abs(m.grade - grade) < Math.abs(closest.grade - grade)) {
+      closest = m;
+    }
+  }
+  return closest.description;
+}
+
+module.exports = { systems, getSystemById, convertToPercent, getColor, getGradeDescription, getAllSystems };
