4791952de7348f7794187e515cb3b184d40fd2c8
Marc Blattmann <marc.blattmann08@gmail.com>
2025-11-03 21:38:00 +0100

    Get Started button

--- page/app/page.tsx
+++ page/app/page.tsx
@@ -34,9 +34,12 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div className="flex flex-col gap-1 justify-center h-1/6 px-5 w-full">
+      <div className="flex flex-col gap-1 justify-center h-2/8 px-5 w-full">
         <h1 className="text-4xl font-bold">Make working with grades easy</h1>
         <h2 className="text-lg">Universal grade conversion and analysis toolkit for international education systems.</h2>
+        <Button size="lg" className="w-40 mt-5 cursor-pointer" asChild>
+          <a>Get Started</a>
+        </Button>
       </div>
     </div>
   );
