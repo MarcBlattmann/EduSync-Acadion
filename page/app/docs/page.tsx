fbd3f87fd232cf954f7428f710a4ededc31b8af7
Marc Blattmann <marc.blattmann08@gmail.com>
2025-11-09 15:10:31 +0100

    Back to normal layout

--- page/app/docs/page.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "@/components/navbar";
-
-export default function DocsPage() {
-  return (
-    <div>
-        Docs
-    </div>
-  );
-}
--- page/app/layout.tsx
+++ page/app/layout.tsx
@@ -1,33 +1,23 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Banner1 } from "@/components/banner1";
+import { ThemeProvider } from "../components/providers";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "EduSync Acadion",
-  description: "Make working with grades easy",
-};
-
-export default function RootLayout({
+export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <ThemeProvider>
+          <div className="w-full h-screen flex flex-col items-center">
+            <Banner1 title={"Version 2.0 is now available!"} description={"Read the full release notes"} linkText={"here"} linkUrl={""} />
+            <div className="flex-1 w-full max-w-360">
+              {children}
+            </div>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
