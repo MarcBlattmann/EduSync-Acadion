90400cfff560947c1bc2f122fd485acc848f1d2f
Marc Blattmann <marc.blattmann08@gmail.com>
2025-11-09 12:05:18 +0100

    Layout fix and docs dont have same layout now

--- page/app/layout.tsx
+++ page/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Banner1 } from "@/components/banner1";
-import { ThemeProvider } from "../components/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,14 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider>
-          <div className="w-full h-screen flex flex-col items-center">
-            <Banner1 title={"Version 2.0 is now available!"} description={"Read the full release notes"} linkText={"here"} linkUrl={""} />
-            <div className="flex-1 w-full max-w-360">
-              {children}
-            </div>
-          </div>
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
