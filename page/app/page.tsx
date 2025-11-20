1bab0ce548c8bc33ac1815b6e5ac253be5974e88
Marc Blattmann <marc.blattmann08@gmail.com>
2025-11-20 20:51:23 +0100

    Remove full height from Home page container
    The 'h-full' class was removed from the main div in the Home page component, allowing the container to adjust its height based on content instead of occupying the full viewport height.

--- page/app/page.tsx
+++ page/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full">
       <div className="flex flex-col gap-1 justify-center mt-10 px-5 w-full">
         <h1 className="text-4xl font-bold">Make working with grades easy</h1>
         <h2 className="text-lg">Universal grade conversion and analysis toolkit for international education systems.</h2>
