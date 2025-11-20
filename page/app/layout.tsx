d6ce86d49779d737d3f3cdc150523171ebfb29de
Marc Blattmann <marc.blattmann08@gmail.com>
2025-11-20 20:50:36 +0100

    Add suppressHydrationWarning to html tag
    Added the suppressHydrationWarning attribute to the <html> tag in MainLayout to address potential hydration mismatches between server and client rendering.

--- page/app/layout.tsx
+++ page/app/layout.tsx
@@ -9,7 +9,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <script src="/theme-init.js" />
       </head>
