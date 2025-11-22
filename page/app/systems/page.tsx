965350f08124ea0fd08b73959a6ab89d8db54c1d
Marc Blattmann <marc.blattmann08@gmail.com>
2025-11-22 23:06:14 +0100

    Picture no longer blocking input field

--- page/app/systems/page.tsx
+++ page/app/systems/page.tsx
@@ -8,7 +8,7 @@ export default function SystemsPage() {
 
     return (
         <div className="relative overflow-hidden h-[calc(100vh-60px)]">
-            <div className="pt-10 px-5 flex flex-col h-full">
+            <div className="pt-10 px-5 flex flex-col h-full relative z-10">
                 <div className="flex flex-col gap-3 mb-8">
                     <h1 className="text-3xl font-bold">Search the Grading systems available</h1>
                     <p className="w-3/4 text-lg">Explore grading systems from countries worldwide, discover their structures, scales, and how they compare to one another.</p>
@@ -17,7 +17,7 @@ export default function SystemsPage() {
                     </div>
                 </div>
             </div>
-            <Image src="/Drawing.png" alt="Background Drawing" className="absolute md:block hidden right-0 bottom-0 w-auto min-w-max" style={{height: '700px'}} width={4000} height={4000} />
+            <Image src="/Drawing.png" alt="Background Drawing" className="absolute md:block hidden right-0 bottom-0 w-auto min-w-max pointer-events-none" style={{height: '700px', zIndex: 0}} width={4000} height={4000} />
         </div>
     );
 }
