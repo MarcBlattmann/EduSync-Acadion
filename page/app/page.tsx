f32d4b950572e171861a93ee5319311ea731a574
Marc Blattmann <marc.blattmann08@gmail.com>
2025-11-28 22:45:28 +0100

    Better mobile landing page

--- page/app/page.tsx
+++ page/app/page.tsx
@@ -6,32 +6,20 @@ import { Code } from "lucide-react";
 
 export default function Home() {
   return (
-    <>
-      <div className="w-full flex justify-center flex-col">
-        <div className="flex w-full justify-center items-center mt-25">
-          <div className="flex flex-col gap-1 justify-center px-5 w-full">
-            <h1 className="text-6xl font-bold">Make working with grades easy</h1>
-            <h2 className="text-2xl w-2/3">A comprehensive set of tools for managing grades, convert values, verify passing status, and perform many other grade-related tasks.</h2>
-            <div className="flex gap-2">
-              <Button size="lg" className="w-40 mt-5 cursor-pointer" asChild>
-                <a href="/docs/Welcome">Get Started</a>
-              </Button>
-              <Button variant="ghost" size="lg" className="w-35 mt-5 cursor-pointer" asChild>
-                <a href="/systems">Search Systems</a>
-              </Button>
-            </div>
-          </div>
+    <div className="relative overflow-hidden h-[calc(100vh-60px)]">
+      <div className="flex flex-col mt-10 gap-1 justify-center px-5 w-full">
+        <h1 className="md:text-6xl text-4xl font-bold">Make working with grades easy</h1>
+        <h2 className="md:text-2xl text-xl md:w-2/3">A comprehensive set of tools for managing grades, convert values, verify passing status, and perform many other grade-related tasks.</h2>
+        <div className="flex gap-2">
+          <Button size="lg" className="w-40 mt-5 cursor-pointer" asChild>
+            <a href="/docs/Welcome">Get Started</a>
+          </Button>
+          <Button variant="ghost" size="lg" className="w-35 mt-5 cursor-pointer" asChild>
+            <a href="/systems">Search Systems</a>
+          </Button>
         </div>
       </div>
-      <div className=" mt-30 px-5">
-        <div>
-          <h1 className="text-3xl font-bold">Grade Convertion</h1>
-          <a className="text-md text-muted-foreground font-semibold">Convert grades between any two grading systems instantly</a>
-          <div className="mt-5">
-            <LiveDemo />
-          </div>
-        </div>
-      </div>
-    </>
+      <Image src="/Drawing.png" alt="Background Drawing" className="absolute right-0 bottom-0 w-auto min-w-max pointer-events-none" style={{height: '700px', zIndex: 0}} width={4000} height={4000} />
+    </div>
   );
 }
